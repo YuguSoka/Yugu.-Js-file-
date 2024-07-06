cfc70394ff7389b6e738ac500df7eb64f8d23ffb
Gai <ggarang24@gmail.com>
2024-07-06 14:04:38 +0800

    dewi changes

--- Dewi.js
+++ Dewi.js
@@ -10,3 +10,10 @@ function myFunction(a, b) {
   return a * b;
 }
 console.log('helloworld')
+
+console.log("Hello World! I Don't Give a Bug");
+function myFunction(g1, g2) {
+    return g1 / g2;
+}
+const value = myFunction(8, 2); // Calling the function
+console.log(value);
