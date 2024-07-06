2dfd6e7543db629d04a6bfb44b3b1aa237f79c20
Gai <ggarang24@gmail.com>
2024-07-06 13:45:18 +0800

    change code

--- Dewi.js
+++ Dewi.js
@@ -1,3 +1,11 @@
 //not yet anything here
 //still not yet anything
 //push from another branch
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+  return a * b;
+}
